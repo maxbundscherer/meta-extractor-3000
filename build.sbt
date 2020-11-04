7bcc1f641b1e2f5ad2562b47992160b6ca3db8f0
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-11-04 16:45:04 +0100

    Set up project

---
+++ build.sbt
@@ -0,0 +1,3 @@
+name := "metadata-extractor-3000"
+version := "0.0.1"
+scalaVersion := "2.13.3"
---
+++ src/main/scala/de/maxbundscherer/metadata/extractor/Main.scala
@@ -0,0 +1,6 @@
+package de.maxbundscherer.metadata.extractor
+
+object Main extends App {
+
+  println("Hello world!")
+}
