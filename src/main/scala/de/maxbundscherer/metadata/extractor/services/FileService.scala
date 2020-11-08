41c1920379313b79e1045db57f1add626cee99f6
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-11-08 20:09:59 +0100

    Prepare json

--- src/main/scala/de/maxbundscherer/metadata/extractor/services/FileService.scala
+++ src/main/scala/de/maxbundscherer/metadata/extractor/services/FileService.scala
@@ -1,12 +1,15 @@
 package de.maxbundscherer.metadata.extractor.services
 
 import de.maxbundscherer.metadata.extractor.utils.Configuration
-
-import com.amazonaws.services.s3.model.S3ObjectSummary
 import org.slf4j.Logger
+
+import scala.util.{ Failure, Success }
 
 class FileService()(implicit log: Logger) extends Configuration {
 
+  import de.maxbundscherer.metadata.extractor.utils.JSON
+
+  import com.amazonaws.services.s3.model.S3ObjectSummary
   import scala.util.Try
   import better.files._
   import java.io.{ File => JFile }
@@ -14,22 +17,27 @@ class FileService()(implicit log: Logger) extends Configuration {
   log.debug("FileService started")
 
   private val fileKeysFilename: String = "fileKeysCache.json"
+  private val json: JSON               = new JSON()
 
   def getCachedFileKeys: Try[Vector[S3ObjectSummary]] = Try(???)
-  def writeCachedFileKeys(data: Vector[S3ObjectSummary]): Try[Unit] =
+
+  /**
+    * Write file keys to cache
+    * @param data fileKeys
+    * @return Try with filePath
+    */
+  def writeCachedFileKeys(data: Vector[S3ObjectSummary]): Try[String] =
     Try {
-
-      //TODO: Use data
-      val content = "testContent"
-
-      File(s"${Config.Global.cacheDirectory}")
-        .createDirectoryIfNotExists()
-
-      File(s"${Config.Global.cacheDirectory}$fileKeysFilename")
-        .createFileIfNotExists()
-        .write(content)
-        .pathAsString
-
+      this.json.convertToJSON(data) match {
+        case Failure(exception) => throw exception
+        case Success(jsonContent) =>
+          File(s"${Config.Global.cacheDirectory}")
+            .createDirectoryIfNotExists()
+          File(s"${Config.Global.cacheDirectory}$fileKeysFilename")
+            .createFileIfNotExists()
+            .write(jsonContent)
+            .pathAsString
+      }
     }
 
 }
