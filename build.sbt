b52a2785cb676795e625ea7325cf474ee3b691ff
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-11-08 20:03:52 +0100

    Add json

--- build.sbt
+++ build.sbt
@@ -5,8 +5,17 @@ scalaVersion := "2.13.3"
 //Logger
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
 
-// AWS SDK
+//AWS SDK
 libraryDependencies += "com.amazonaws" % "aws-java-sdk" % "1.11.896"
 
-// Better Files
+//Better Files
 libraryDependencies += "com.github.pathikrit" %% "better-files" % "3.9.1"
+
+//JSON
+val circeVersion = "0.12.3"
+
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
--- src/main/scala/de/maxbundscherer/metadata/extractor/services/FileService.scala
+++ src/main/scala/de/maxbundscherer/metadata/extractor/services/FileService.scala
@@ -4,13 +4,32 @@ import de.maxbundscherer.metadata.extractor.utils.Configuration
 
 import com.amazonaws.services.s3.model.S3ObjectSummary
 import org.slf4j.Logger
-import scala.util.Try
 
 class FileService()(implicit log: Logger) extends Configuration {
 
+  import scala.util.Try
+  import better.files._
+  import java.io.{ File => JFile }
+
   log.debug("FileService started")
 
-  def getCachedFileKeys: Try[Vector[S3ObjectSummary]]               = Try(???)
-  def writeCachedFileKeys(data: Vector[S3ObjectSummary]): Try[Unit] = Try(???)
+  private val fileKeysFilename: String = "fileKeysCache.json"
+
+  def getCachedFileKeys: Try[Vector[S3ObjectSummary]] = Try(???)
+  def writeCachedFileKeys(data: Vector[S3ObjectSummary]): Try[Unit] =
+    Try {
+
+      //TODO: Use data
+      val content = "testContent"
+
+      File(s"${Config.Global.cacheDirectory}")
+        .createDirectoryIfNotExists()
+
+      File(s"${Config.Global.cacheDirectory}$fileKeysFilename")
+        .createFileIfNotExists()
+        .write(content)
+        .pathAsString
+
+    }
 
 }
