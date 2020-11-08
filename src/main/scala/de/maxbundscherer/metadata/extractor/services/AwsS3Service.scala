fc91197a34b810f58d359090a01c1ac6777748a8
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-11-08 20:42:16 +0100

    Add cache

--- src/main/scala/de/maxbundscherer/metadata/extractor/services/AwsS3Service.scala
+++ src/main/scala/de/maxbundscherer/metadata/extractor/services/AwsS3Service.scala
@@ -14,14 +14,7 @@ class AwsS3Service(fileService: FileService)(implicit log: Logger) extends Confi
 
   log.debug("AwsS3Service started")
 
-  this.awsS3Client.listBuckets match {
-    case Failure(exception) =>
-      log.error(s"Got exception in listBuckets (${exception.getLocalizedMessage})")
-    case Success(data) =>
-      data.foreach(d => log.info(s"Got item in listBuckets (${d.getName})"))
-  }
-
-  private val cachedItems: Option[Vector[S3ObjectSummary]] =
+  private lazy val cachedItems: Option[Vector[S3ObjectSummary]] =
     this.fileService.getCachedFileKeys match {
       case Failure(exception) =>
         log.info(s"No cached listFiles (${exception.getLocalizedMessage})")
@@ -31,17 +24,30 @@ class AwsS3Service(fileService: FileService)(implicit log: Logger) extends Confi
         Some(data)
     }
 
-  this.awsS3Client.listFiles(Config.AwsClients.S3.bucketName, cachedItems = cachedItems) match {
+  private lazy val fileKeys: Vector[S3ObjectSummary] =
+    this.awsS3Client.listFiles(Config.AwsClients.S3.bucketName, cachedItems = cachedItems) match {
+      case Failure(exception) =>
+        log.error(s"Got exception in listFiles (${exception.getLocalizedMessage})")
+        ???
+      case Success(fileKeys) =>
+        log.info(s"Got ${fileKeys.size} items in listFiles")
+        this.fileService.writeCachedFileKeys(fileKeys) match {
+          case Failure(exception) =>
+            log.warn(s"Exception in cache update (${exception.getLocalizedMessage})")
+            ???
+          case Success(filePath) =>
+            log.info(s"Cached updated ($filePath)")
+            fileKeys
+        }
+    }
+
+  this.awsS3Client.listBuckets match {
     case Failure(exception) =>
-      log.error(s"Got exception in listFiles (${exception.getLocalizedMessage})")
-    case Success(fileKeys) =>
-      log.info(s"Got ${fileKeys.size} items in listFiles")
-      this.fileService.writeCachedFileKeys(fileKeys) match {
-        case Failure(exception) =>
-          log.warn(s"Exception in cache update (${exception.getLocalizedMessage})")
-        case Success(filePath) => log.info(s"Cached updated ($filePath)")
-      }
-    //data.foreach(d => log.info(s"Got item in listFiles (${d.getKey})"))
+      log.error(s"Got exception in listBuckets (${exception.getLocalizedMessage})")
+    case Success(data) =>
+      data.foreach(d => log.info(s"Got item in listBuckets (${d.getName})"))
   }
+
+  this.fileKeys.take(10).foreach(fileKey => log.debug(s"Got fileKey (${fileKey.getKey})"))
 
 }
